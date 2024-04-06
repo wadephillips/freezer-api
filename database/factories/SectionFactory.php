19d588f075704b76abc79cd5d9e3ff44680c66e6
Wade Phillips <hello@wadelp.com>
2024-04-06 06:16:42 -0800

    Added SectionFactory

---
+++ database/factories/SectionFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Section;
+use App\Models\Space;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class SectionFactory extends Factory
+{
+
+    protected $model = Section::class;
+
+    public function definition(): array
+    {
+
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->text(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'space_id' => Space::factory(),
+        ];
+    }
+
+}
