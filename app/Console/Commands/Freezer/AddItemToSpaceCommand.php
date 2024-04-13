d50f11551449b560298405aa2fbd4ddedd15ca9e
Wade Phillips <hello@wadelp.com>
2024-04-13 05:45:35 -0800

    Fixed functionality of AddItemToSpaceCommand.php

--- app/Console/Commands/Freezer/AddItemToSpaceCommand.php
+++ app/Console/Commands/Freezer/AddItemToSpaceCommand.php
@@ -8,6 +8,7 @@ use App\Models\Section;
 use App\Models\Space;
 use Illuminate\Console\Command;
 
+use function Laravel\Prompts\confirm;
 use function Laravel\Prompts\search;
 use function Laravel\Prompts\select;
 use function Laravel\Prompts\text;
@@ -21,25 +22,31 @@ class AddItemToSpaceCommand extends Command
 
     public function handle(AddItemToSectionAction $addItemToSectionAction): void
     {
-        $spaces = Space::select(['id', 'name'])->with(['sections'])->get();
-        $spaceChoice = select('Where would you like to store your item?', $spaces->pluck('name', 'id'));
-        $space = $spaces[ $spaceChoice - 1 ];
 
-        $sections = $space->sections;
-        $sectionChoice = select('Which section does it belong in?', $sections->pluck('name', 'id'));
-        $section = $sections[ $sectionChoice - 1 ];
+            $spaces = Space::select(['id', 'name'])->with(['sections'])->get()->keyBy('id');
+            $spaceChoice = select('Where would you like to store your item?', $spaces->pluck('name', 'id'));
 
-        $item = search(
-            "Which item would you like to add to {$space->name} - {$section->name}?",
-            fn (string $value) => strlen($value) > 0
-                ? Item::where('name', 'like', "%{$value}%")->pluck('name', 'id')->all()
-                : []
-        );
+            $space = $spaces[$spaceChoice];
+            $sections = $space->sections->keyBy('id');
+            $sectionChoice = select('Which section does it belong in?', $sections->pluck('name', 'id'));
+            $section = $sections[ $sectionChoice ];
+        do {
+            $itemSelection = search(
+                "Which item would you like to add to {$space->name} - {$section->name}?",
+                fn(string $value) => strlen($value) > 0 ? Item::where('name', 'like', "%{$value}%")->pluck(
+                    'name',
+                    'id'
+                )->all() : []
+            );
+            $item = Item::find($itemSelection);
+            $quantity = text('How many?');
 
-        $quantity = text('How many?');
+            info("{$quantity} of {$item->name} have been added to {$space->name} - {$section->name}");
 
-        $addItemToSectionAction->execute($item, $section, $quantity);
-        //todo resume
+            $addItemToSectionAction->execute($item, $section, $quantity);//todo resume
+            $rerun = confirm("Would you like to add another item to {$space->name} - {$section->name}");
+
+        } while ($rerun);
     }
 
 }
