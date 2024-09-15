a99cc4aad51fd8036b1bd020aad609dc6615d72b
Wade Phillips <hello@wadelp.com>
2024-09-15 06:51:21 -0800

    Built out destroy method on SectionController and tested

---
+++ app/Http/Controllers/SpaceSectionController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Section;
+use Illuminate\Http\Request;
+
+class SpaceSectionController extends Controller
+{
+
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(Section $section)
+    {
+    }
+
+    public function update(Request $request, Section $section)
+    {
+    }
+
+    public function destroy(Section $section)
+    {
+    }
+
+}
---
+++ tests/Feature/API/SpaceSectionTest.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('retrieves all Sections for a specific Space', function () {
+
+})->todo();
+it('does not retrieve sections that do not belong to the specified space', function () {})->todo();
